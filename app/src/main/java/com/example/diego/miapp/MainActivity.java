7cfba58029a87b5d783c0ef2b9c6fd802b513cb4
diego <diemp94@hotmail.com>
2017-11-14 13:34:06 -0600

    cambio del 14

--- app/src/main/java/com/example/diego/miapp/MainActivity.java
+++ app/src/main/java/com/example/diego/miapp/MainActivity.java
@@ -2,40 +2,66 @@ package com.example.diego.miapp;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.util.Log;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Switch;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.etNombre)
+    EditText etNombre;
+    @BindView(R.id.etApellidos)
+    EditText etApellidos;
+    @BindView(R.id.etMail)
+    EditText etMail;
+    @BindView(R.id.etTelefono)
+    EditText etTelefono;
+    @BindView(R.id.swTerminos)
+    Switch swTerminos;
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+    @BindView(R.id.nav_view)
+    NavigationView navView;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        int a=1;
-        Log.d("a: ",String.valueOf(a));
-        int b=2;
-        Log.d("b: ",String.valueOf(b));
-        int c= a+b;
-        Log.d("c: ",String.valueOf(c));
+        int a = 1;
+        Log.d("a: ", String.valueOf(a));
+        int b = 2;
+        Log.d("b: ", String.valueOf(b));
+        int c = a + b;
+        //Log.d("c: ",String.valueOf(c));
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Comprobando que funciona nuestro control de versiones 2", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -105,5 +131,9 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @OnClick(R.id.btnGuardar)
+    public void onViewClicked() {
     }
 }
